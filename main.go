3718cfee99c83590ec25ea0e8f281b5c6f72b978
Negar <negarsaljooghi@gmail.com>
2026-07-13 16:40:01 +0330

    Improve user input example

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var name string
+	var age int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
+
+	fmt.Println("Hello", name)
+	fmt.Println("You are", age, "years old!")
+
+}
